a13e6a797dc040e6721c8cb5c2c3c7b92660c77b
Zechariah <zechariahtan144@gmail.com>
2022-01-16 21:56:47 +0800

    Using mouse events instead

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -7,45 +7,41 @@ const Cube = (): JSX.Element => {
 	const [position, setPosition] = useState<iRotation>({ x: 0, y: 0 })
 	const [rotations, setRotations] = useState<iRotation[]>([])
 
-	const handleDrag = (e: React.DragEvent<HTMLDivElement>) => {
-		if (e.clientX === 0 && e.clientY === 0) return
-
-		const difference = { x: e.clientX - position.x, y: e.clientY - position.y }
-
-		const rotation = rotations.at(-1)!
-		setRotations([
-			...rotations.slice(0, -1),
-			{ x: rotation.x + difference.x, y: rotation.y + difference.y }
-		])
-
-		setPosition({ x: e.clientX, y: e.clientY })
-	}
-
-	const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+	const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
 		setDragging(true)
 		setPosition({ x: e.clientX, y: e.clientY })
 		setRotations([...rotations, { x: 0, y: 0 }])
+	}
 
-		if (e.dataTransfer) {
-			const img = new Image()
-			img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs="
-			e.dataTransfer.setDragImage(img, 0, 0)
-			e.dataTransfer.dropEffect = "move"
+	const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		if (dragging) {
+			if (e.clientX === 0 && e.clientY === 0) return
+
+			const difference = { x: e.clientX - position.x, y: e.clientY - position.y }
+
+			const rotation = rotations.at(-1)!
+			setRotations([
+				...rotations.slice(0, -1),
+				{ x: rotation.x + difference.x, y: rotation.y + difference.y }
+			])
+
+			setPosition({ x: e.clientX, y: e.clientY })
 		}
 	}
 
-	const handleDragEnd = () => {
-		setDragging(false)
-		setPosition({ x: 0, y: 0 })
+	const handleMouseUp = () => {
+		if (dragging) {
+			setPosition({ x: 0, y: 0 })
+			setDragging(false)
+		}
 	}
 
 	return (
 		<div
 			className={`drag-area ${dragging ? "dragging" : ""}`}
-			draggable={true}
-			onDragStart={handleDragStart}
-			onDrag={handleDrag}
-			onDragEnd={handleDragEnd}>
+			onMouseDown={handleMouseDown}
+			onMouseMove={handleMouseMove}
+			onMouseUp={handleMouseUp}>
 			<div className="cube-wrapper">
 				<div className="cube">
 					{rotations.reduce(
