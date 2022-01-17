f69c3a3ea378beb7226d9eff53dacff3819f7cc9
Zechariah Tan <zechariahtan144@gmail.com>
2022-01-17 14:12:38 +0800

    Change to rotate and snap

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -10,70 +10,56 @@ interface Props {
 const Cube = (props: Props): JSX.Element => {
 	const { cubeData } = props
 
-	const [dragging, setDragging] = useState(false)
-	const [position, setPosition] = useState<iRotation>({ x: 0, y: 0 })
-	const [rotations, setRotations] = useState<iRotation[]>([{ x: 90, y: 60 }])
+	const [cursor, setCursor] = useState<iRotation>({ x: 0, y: 0 })
+	const [drag, setDrag] = useState<iRotation | null>(null)
 
 	const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
 		if (e.button === 1) {
-			setDragging(true)
-			setPosition({ x: e.clientX, y: e.clientY })
-			setRotations([...rotations, { x: 0, y: 0 }])
+			setDrag({ x: 0, y: 0 })
+			setCursor({ x: e.clientX, y: e.clientY })
 		}
 	}
 
 	const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-		if (dragging) {
+		if (drag) {
 			if (e.clientX === 0 && e.clientY === 0) return
 
-			const difference = { x: e.clientX - position.x, y: e.clientY - position.y }
-
-			const rotation = rotations.at(-1)!
-			setRotations([
-				...rotations.slice(0, -1),
-				{ x: rotation.x + difference.x, y: rotation.y + difference.y }
-			])
-
-			setPosition({ x: e.clientX, y: e.clientY })
+			setDrag({ x: drag.x + e.clientX - cursor.x, y: drag.y + e.clientY - cursor.y })
+			setCursor({ x: e.clientX, y: e.clientY })
 		}
 	}
 
 	const handleMouseUp = () => {
-		if (dragging) {
-			setPosition({ x: 0, y: 0 })
-			setDragging(false)
+		if (drag) {
+			setDrag(null)
+			setCursor({ x: 0, y: 0 })
 		}
 	}
 
 	return (
 		<div
-			className={`drag-area ${dragging ? "dragging" : ""}`}
+			className={`drag-area ${drag ? "dragging" : ""}`}
 			onMouseDown={handleMouseDown}
 			onMouseMove={handleMouseMove}
 			onMouseUp={handleMouseUp}>
 			<div className="cube-wrapper">
 				<div className="cube">
-					{rotations.reduce(
-						(a, r) => (
-							<div
-								className="rotation-wrapper"
-								style={{
-									transform: `rotateX(${-r.y * 0.5}deg) rotateY(${r.x * 0.5}deg)`
-								}}>
-								{a}
-							</div>
-						),
-						<>
-							{cubeData.map(({ correct, current }) => (
-								<Square key={`${correct.face}-${correct.position}`} {...current} />
+					<div
+						className="rotation-wrapper"
+						style={{
+							transform: drag
+								? `rotateX(${-drag.y * 0.5}deg) rotateY(${drag.x * 0.5}deg)`
+								: ""
+						}}>
+						{cubeData.map(({ correct, current }) => (
+							<Square key={`${correct.face}-${correct.position}`} {...current} />
+						))}
+						{Array(12)
+							.fill(0)
+							.map((_, i) => (
+								<Surface key={i} i={i} />
 							))}
-							{Array(12)
-								.fill(0)
-								.map((_, i) => (
-									<Surface key={i} i={i} />
-								))}
-						</>
-					)}
+					</div>
 				</div>
 			</div>
 		</div>
