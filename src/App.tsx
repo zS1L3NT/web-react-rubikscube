6dc1bf32fc3509ddc546e21a454297e855f890db
Zechariah <zechariahtan144@gmail.com>
2022-01-21 02:12:52 +0800

    Prevent cube twisting, not time and too complex

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import Cube from "./components/Cube"
 import defaultCubeData from "./json/default-cube-data.json"
 import DragArea from "./components/DragArea"
 import { iCubeData } from "./types.d"
-import { useState } from "react"
 
 const App = () => {
-	const [cubeData, setCubeData] = useState<iCubeData>(defaultCubeData as iCubeData)
-
-	return <DragArea cubeData={cubeData} />
+	return <DragArea cubeData={defaultCubeData as iCubeData} />
 }
 
 export default App
